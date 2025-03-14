5ac1d5d2799bec4a6c363b42af9ddf664eea70d9
J4AI <Ancientshi@gmail.com>
2025-03-14 11:16:57 +1100

    all

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 gpt_log_path='logs/gpt_response.log'
 model_cache_dir="./model"
 proj_path = "/Users/j4-ai/workspace/OCG"
-openai_api_key ='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'
-erk_key='3bcbc1e3-dbba-4fb8-bda5-9a91efe55d36'
-silicon_flow_key='sk-ucgmnazpxdukpbdlyqllxqyjbrxbjiukdewhgguhecrdlgmi' 
+openai_api_key =''
+erk_key=''
+silicon_flow_key='' 
